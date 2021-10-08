53ddb2a2fb7e02e8ab4b3c1ff49495581cc3dda6
Mihai Blaga <legolas8911@gmail.com>
2021-10-09 00:46:48 +0200

    test

--- src/__tests__/genericComponents.test.js
+++ src/__tests__/genericComponents.test.js
@@ -2,24 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 
-import Carousel from '../components/generic/Carousel';
 import DetailsLabel from '../components/generic/DetailsLabel';
 import DetailsList from '../components/generic/DetailsList';
 import DetailsPlaceholder from '../components/generic/DetailsPlaceholder';
 import SearchField from '../components/generic/SearchField';
-import VideoModal from '../components/generic/VideoModal';
 import mockData from '../utils/mockData';
 
 /* global document */
 
 // keeping the code dry
 const tests = [
-    { name: 'Carousel', component: <Carousel title="test" getDataFunction={() => Promise.resolve()} /> },
     { name: 'DetailsLabel', component: <DetailsLabel label="test">Label</DetailsLabel> },
     { name: 'DetailsList', component: <DetailsList isMovie data={mockData.detailsList} /> },
     { name: 'DetailsPlaceholder', component: <DetailsPlaceholder /> },
     { name: 'SearchField', component: <SearchField /> },
-    { name: 'VideoModal', component: <VideoModal /> },
 ];
 
 tests.forEach((test) =>
